47a15ac4d9b6c9e5381a36f9c3d4c0691f1d2d39
Sam <luismendoza.samuel@gmail.com>
2017-11-29 23:18:29 -0500

    Archivos base para el proyecto
    Un login simple, comprueba usuario y contraseña y notifica si el usuario no existe o si la contraseña es incorrecta.

---
+++ src/proyectoinnovacionintranet/ProyectoInnovacionIntranet.java
@@ -0,0 +1,25 @@
+
+package proyectoinnovacionintranet;
+
+import java.sql.Connection;
+
+/**
+ *
+ * @author Alisoft
+ */
+public class ProyectoInnovacionIntranet {
+
+    /**
+     * @param args the command line arguments
+     
+    public static void main(String[] args){
+        try {
+            Connection c = conexion.Conexion.getInstancia().getConn();
+            System.out.println("conectado");
+        } catch (Exception ex) {
+            System.out.println("error al conectar");
+        }
+        
+    }
+    */
+}
